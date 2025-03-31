3347540e95cf8621de5d999077e9a7747c828ce9
Nuran Ramis <nuranrms@gmail.com>
2025-03-31 17:53:29 +0200

    setup-of-client-side

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -27,6 +27,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
