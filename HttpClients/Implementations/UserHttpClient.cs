3dba9f90e96dc727b072e7baa83a9cdffd5add95
Nuran Ramis <nuranrms@gmail.com>
2025-04-03 15:35:47 +0200

    blazor-view-users

--- HttpClients/Implementations/UserHttpClient.cs
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -27,4 +27,27 @@ public class UserHttpClient : IUserService
         User user = JsonSerializer.Deserialize<User>(result)!;
         return user;
     }
+
+    public async Task<IEnumerable<User>> GetAsync(string? usernameContains = null)
+    {
+        string uri = "/users";
+        if (!string.IsNullOrEmpty(usernameContains))
+        {
+            uri += $"?username={usernameContains}";
+        }
+
+        HttpResponseMessage response = await client.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+
+        }
+
+        IEnumerable<User> users = JsonSerializer.Deserialize<IEnumerable<User>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
+    }
 }
