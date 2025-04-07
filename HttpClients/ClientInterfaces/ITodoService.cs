a156ce3d5b567f4b6e70b6b7ebc8c017d9736efd
Nuran Ramis <nuranrms@gmail.com>
2025-04-07 22:15:54 +0200

    edit todo

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -9,4 +9,8 @@ public interface ITodoService
     
     Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains);
     
+    Task UpdateAsync(TodoUpdateDto dto);
+    
+    Task<TodoBasicDto> GetByIdAsync(int id);
+    
 }
